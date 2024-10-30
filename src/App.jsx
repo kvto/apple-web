e4b58c00ea2acc2ae99263207a17d604df8bef1b
Kevin J. Monter Zea. <kjms.15987@gmail.com>
2024-10-30 17:26:41 -0500

    Update 30-10-2024

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
 import Navbar from "./components/Navbar"
-
+import Features from "./components/Features"
 
 const  App = () => {
 
@@ -12,6 +12,7 @@ const  App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
     </main>
   )
 }
